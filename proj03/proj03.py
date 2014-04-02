dd05a93eb3c8063b5d4afc3883e2c7ff6e95bbee
lamasu <rafaerthal@gmail.com>
2014-04-02 02:32:18 -0300

    proj03 report

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,9 +1,14 @@
+from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
 from sklearn.neighbors import NeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+
 import random
 import numpy as np
+
+def print_result(rates):
+    print int(np.mean(rates) * 100), '+/-', int(np.std(rates) * 100)
 
 def fold(quantity, data):
     size = len(data[0]) / quantity
@@ -53,30 +58,31 @@ def rates(data, grid, Classifier):
     return rates
 
 data = read_data("./data.csv")
-pca = PCA(n_components=12)
+data = (normalize(data[0]).tolist(), data[1])
+pca  = PCA(n_components=12)
 pca.fit(data[0])
 data_pca = (pca.transform(data[0]).tolist(), data[1])
 
 kneighbors_grid = [1,3,5,11,21,31]
 kneighbors_rates = rates(data, kneighbors_grid, lambda K: NeighborsClassifier(n_neighbors=K))
 kneighbors_pca_rates = rates(data_pca, kneighbors_grid, lambda K: NeighborsClassifier(n_neighbors=K))
-print np.mean(kneighbors_rates), '+/-', np.std(kneighbors_rates)
-print np.mean(kneighbors_pca_rates), '+/-', np.std(kneighbors_pca_rates)
+print_result(kneighbors_rates)
+print_result(kneighbors_pca_rates)
 
 svm_linear_grid = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
 svm_linear_rates = rates(data, svm_linear_grid, lambda C: SVC(C=C, kernel='linear'))
 svm_linear_pca_rates = rates(data_pca, svm_linear_grid, lambda C: SVC(C=C, kernel='linear'))
-print np.mean(svm_linear_rates), '+/-', np.std(svm_linear_rates)
-print np.mean(svm_linear_pca_rates), '+/-', np.std(svm_linear_pca_rates)
+print_result(svm_linear_rates)
+print_result(svm_linear_pca_rates)
 
 svm_rbf_grid    = [[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000]]
 svm_rbf_rates = rates(data, svm_rbf_grid, lambda C: SVC(C=C[0], gamma=C[1], kernel='rbf'))
 svm_rbf_pca_rates = rates(data_pca, svm_rbf_grid, lambda C: SVC(C=C[0], gamma=C[1], kernel='rbf'))
-print np.mean(svm_rbf_rates), '+/-', np.std(svm_rbf_rates)
-print np.mean(svm_rbf_pca_rates), '+/-', np.std(svm_rbf_pca_rates)
+print_result(svm_rbf_rates)
+print_result(svm_rbf_pca_rates)
 
 randforest_grid = [2,3,5,10,20,40,60]
 randforest_rates = rates(data, randforest_grid, lambda C: RandomForestClassifier(max_depth=C))
 randforest_pca_rates = rates(data_pca, randforest_grid, lambda C: RandomForestClassifier(max_depth=C))
-print np.mean(randforest_rates), '+/-', np.std(randforest_rates)
-print np.mean(randforest_pca_rates), '+/-', np.std(randforest_pca_rates)
+print_result(kneighbors_rates)
+print_result(kneighbors_pca_rates)
