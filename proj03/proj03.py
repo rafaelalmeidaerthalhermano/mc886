3687fa151c4127c5ee2201677ef68fcae9f98e21
lamasu <rafaerthal@gmail.com>
2014-04-03 15:29:23 -0300

    proj03 report

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,27 +1,17 @@
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.neighbors import NeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-
-import random
 import numpy as np
 
 def print_result(rates):
     print int(np.mean(rates) * 100), '+/-', int(np.std(rates) * 100)
 
-def fold(quantity, data):
-    size = len(data[0]) / quantity
-    for i in range(0, quantity):
-        p     = i * size
-        train = (data[0][0:p] + data[0][p + size:], data[1][0:p] + data[1][p + size:])
-        test  = (data[0][p: p + size], data[1][p: p + size])
-        yield (train, test)
-
 def read_data(file_name):
     cast_int = lambda x: 1 if x == 'M' else 0
     lines    = open(file_name,'r').readlines()
-    random.shuffle(lines)
     records  = [line[0:-1].split(',') for line in lines]
     data     = [map(float, record[0:-1]) for record in records]
     labels   = [cast_int(record[-1]) for record in records]
@@ -39,7 +29,15 @@ def best_grid(data, grid, Classifier):
     highest_matches = 0
     for K in grid:
         matches = 0
-        for train, test in fold(5, data):
+        for train_index, test_index in StratifiedKFold(data[1], 5):
+            train = (
+                [i for j, i in enumerate(data[0]) if j not in train_index],
+                [i for j, i in enumerate(data[1]) if j not in train_index]
+            )
+            test  = (
+                [i for j, i in enumerate(data[0]) if j not in test_index],
+                [i for j, i in enumerate(data[1]) if j not in test_index]
+            )
             classifier = Classifier(K)
             classifier.fit(train[0], train[1])
             matches += correct_predictions(classifier, test)
@@ -49,40 +47,58 @@ def best_grid(data, grid, Classifier):
     return best_k
 
 def rates(data, grid, Classifier):
-    rates = []
-    for train, test in fold(5, data):
-        classifier = Classifier(best_grid(train, grid, Classifier))
+    rates    = []
+    for train_index, test_index in StratifiedKFold(data[1], 5):
+        train          = (
+            [i for j, i in enumerate(data[0]) if j not in train_index],
+            [i for j, i in enumerate(data[1]) if j not in train_index]
+        )
+        test           = (
+            [i for j, i in enumerate(data[0]) if j not in test_index],
+            [i for j, i in enumerate(data[1]) if j not in test_index]
+        )
+        hiperparameter = best_grid(train, grid, Classifier)
+        classifier     = Classifier(hiperparameter)
         classifier.fit(train[0], train[1])
         rate = correct_predictions(classifier, test) / float(len(test[0]))
         rates.append(rate)
     return rates
 
+
 data = read_data("./data.csv")
 data = (normalize(data[0]).tolist(), data[1])
-pca  = PCA(n_components=12)
-pca.fit(data[0])
-data_pca = (pca.transform(data[0]).tolist(), data[1])
+
+data_pca = read_data("./data.csv")
+data_pca = (normalize(data_pca[0]).tolist(), data_pca[1])
+
+pca  = PCA()
+pca.fit(data_pca[0])
+data_pca = (pca.transform(data_pca[0]).tolist(), data_pca[1])
 
 kneighbors_grid = [1,3,5,11,21,31]
+svm_linear_grid = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+svm_rbf_grid = []
+for i in svm_linear_grid:
+    for j in svm_linear_grid:
+         svm_rbf_grid.append([i,j])
+randforest_grid = [2,3,5,10,20,40,60]
+
 kneighbors_rates = rates(data, kneighbors_grid, lambda K: NeighborsClassifier(n_neighbors=K))
-kneighbors_pca_rates = rates(data_pca, kneighbors_grid, lambda K: NeighborsClassifier(n_neighbors=K))
 print_result(kneighbors_rates)
+kneighbors_pca_rates = rates(data_pca, kneighbors_grid, lambda K: NeighborsClassifier(n_neighbors=K))
 print_result(kneighbors_pca_rates)
 
-svm_linear_grid = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
 svm_linear_rates = rates(data, svm_linear_grid, lambda C: SVC(C=C, kernel='linear'))
-svm_linear_pca_rates = rates(data_pca, svm_linear_grid, lambda C: SVC(C=C, kernel='linear'))
 print_result(svm_linear_rates)
+svm_linear_pca_rates = rates(data_pca, svm_linear_grid, lambda C: SVC(C=C, kernel='linear'))
 print_result(svm_linear_pca_rates)
 
-svm_rbf_grid    = [[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000],[0.001, 0.001],[0.01, 0.01],[0.1, 0.1],[1, 1],[10, 10],[100, 100],[1000, 1000],[10000, 10000]]
 svm_rbf_rates = rates(data, svm_rbf_grid, lambda C: SVC(C=C[0], gamma=C[1], kernel='rbf'))
-svm_rbf_pca_rates = rates(data_pca, svm_rbf_grid, lambda C: SVC(C=C[0], gamma=C[1], kernel='rbf'))
 print_result(svm_rbf_rates)
+svm_rbf_pca_rates = rates(data_pca, svm_rbf_grid, lambda C: SVC(C=C[0], gamma=C[1], kernel='rbf'))
 print_result(svm_rbf_pca_rates)
 
-randforest_grid = [2,3,5,10,20,40,60]
 randforest_rates = rates(data, randforest_grid, lambda C: RandomForestClassifier(max_depth=C))
-randforest_pca_rates = rates(data_pca, randforest_grid, lambda C: RandomForestClassifier(max_depth=C))
 print_result(kneighbors_rates)
+randforest_pca_rates = rates(data_pca, randforest_grid, lambda C: RandomForestClassifier(max_depth=C))
 print_result(kneighbors_pca_rates)
